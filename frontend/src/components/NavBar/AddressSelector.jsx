cc3d087e71f35ff74b91f7d218e9ba9676188f4f
JoeyVCU <fondacoj@vcu.edu>
2026-04-06 14:01:06 -0400

    Adding 1st Version UglyBuild Img

--- frontend/src/components/NavBar/AddressSelector.jsx
+++ frontend/src/components/NavBar/AddressSelector.jsx
@@ -2,6 +2,6 @@ export default function AddressSelector() {
     // Need to add a popup with a location selection tool
         // See amazons "deliver to x" feature in their navbar
         // Should have a different popup depending on if the user is signed in or not
-            // see amazon for more examples
+            // see amazon for more examples of this
   return <div>AddressSelector</div>;
 }
