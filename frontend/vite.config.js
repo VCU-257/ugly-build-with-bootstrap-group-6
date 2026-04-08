13f8c726cd2864b8e3589db2096515e4e6997614
JoeyVCU <fondacoj@vcu.edu>
2026-04-08 15:18:07 -0400

    Adding deploy.yml

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url'
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-  base: '/ugly-build-with-bootstrap-group-6/frontend', 
+  base: '/ugly-build-with-bootstrap-group-6/', 
   plugins: [
     react({
       include: '**/*.{jsx,js}',
