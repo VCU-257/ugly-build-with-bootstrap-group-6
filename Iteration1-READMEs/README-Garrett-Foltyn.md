4024cc556751ff3e68414fcde535842ac44f8105
Garrett Foltyn <garrettfoltyn@gmail.com>
2026-04-08 15:43:39 -0400

    ....

---
+++ Iteration1-READMEs/README-Garrett-Foltyn.md
@@ -0,0 +1,27 @@
+# Student Name: [Your First and Last Name]
+
+## 1. My Assigned Work
+*List the specific HTML files, screens, or components you built based on your team's assignments in Table 3.*
+
+I built the Checkout and Order Confirmation pages.
+## 2. Bootstrap Implementation
+*Briefly explain the standard components you used for your assigned areas. [cite_start]Did you stick to the plan from Table 1?*
+* **Components Used:** *Cards to contain all the other elements, Description Lists to list and separate all the different parts of the order, and Buttons to move allow easy movement between pages.*
+
+All components I used where from Table 1 except buttons.
+
+
+## 3. Technical Challenges & Solutions
+[cite_start]*Look back at Table 4: Technical Risks. Did you build any of those risky components? How did you solve the challenge? If you didn't face those specific risks, what was the hardest part of your assigned work?*
+
+I did not build anything from table 4.
+## 4. AI / LLM Usage
+*Did you use an AI tool to help write or debug your code?*
+* **What I asked the AI:** *(e.g., "How do I make a sticky footer in Bootstrap 5?")*
+* **How it helped & What I learned:** *(Explain the solution it provided and how you ensured you understood the code).*
+
+I did not use any AI, everything I added I pulled from the bootstrap website and then modified.
+
+## 5. Live Site Link
+*Provide the GitHub Pages link to the specific page(s) you built.*
+* **Live URL:** [Insert Link Here]
--- frontend/src/pages/Checkout.jsx
+++ frontend/src/pages/Checkout.jsx
@@ -1,5 +1,48 @@
 import SectionOutline from '../components/SectionOutline.jsx'
-
 export default function Checkout() {
-  return <SectionOutline label="Checkout page" />
+  return (
+      <SectionOutline label="Checkout">
+          <div className="card">
+
+              <div class="card-header">
+                  <ul class="list-group list-group-flush">
+              <li class="list-group-item">
+                  <h4>Items</h4>
+                <dt class="col-sm-8"> Painting</dt>
+                <dd class="col-sm-4"> $67.00 </dd>
+                <dt class="col-sm-8"> Giant Metal Sculpture</dt>
+                <dd class="col-sm-4"> $1000.00 </dd>
+                </li>
+                <li class="list-group-item">
+                    <h4>Shipping</h4>
+                <dt class="col-sm-8"> Free</dt>
+                </li>
+                <li class="list-group-item">
+                  <h4>Address</h4>
+                <p>123 ABC Street<br />Neverland, OH 23220</p>
+
+                </li>
+                <li class="list-group-item">
+                  <h4>Payment Method</h4>
+                <dt class="col-sm-8"> Card Type:</dt>
+                <dd class="col-sm-4"> Visa ending in 1234 </dd>
+
+                </li>
+                <li class="list-group-item">
+                  <h4>Estimated Delivery Date</h4>
+                <p>Friday, Oct 13th</p>
+
+                </li>
+                </ul>
+              </div>
+          <a class="btn btn-primary" href="/order-confirmation" role="button">Checkout</a>
+
+          </div>
+
+
+
+      </SectionOutline>
+
+
+      )
 }
